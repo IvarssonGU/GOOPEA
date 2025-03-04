f05b4db3fb899929519068414d57f1f74d48e9e7
PJWahlgren <patrik.johan.wahlgren@gmail.com>
2025-03-04 10:18:11 +0100

    Connected everything to rust

--- editor/src/lib.rs
+++ editor/src/lib.rs
@@ -5,9 +5,30 @@ use wasm_bindgen::prelude::*;
 #[wasm_bindgen]
 extern "C" {
     fn alert(s: &str);
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+    // #[wasm_bindgen(raw_module = "/../www/index.js")]
+    // fn to_output_box(s : &str);    
 }
+
+#[wasm_bindgen(module = "/src/jstest.js")]
+extern "C" {
+    fn testprint();
+}
+
+#[wasm_bindgen(raw_module = "../www/index.js")]
+extern "C" {
+    fn to_output_box(s : &str);
+}
+
 
 #[wasm_bindgen]
 pub fn greet() {
     alert("Hello, editor!");
+}
+#[wasm_bindgen]
+pub fn process_text(text : &str){
+    // log(text)
+    to_output_box(text);
+    testprint()
 }
