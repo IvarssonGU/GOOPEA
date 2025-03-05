effa4a18ecdb532874735cfb0e3107c15d40d6ce
ellalindstroem <ellalind@student.chalmers.se>
2025-03-05 13:39:58 +0100

    fixed formatting/style

--- editor/static/js/index.js
+++ editor/static/js/index.js
@@ -1,11 +1,19 @@
 let user_code_textarea = document.getElementById("editor");
 let code_output_textarea = document.getElementById("output");
+// let cm = CodeMirror.fromTextArea(user_code_textarea);
+// cm.setSize("100%", "100%");
+
+// document.addEventListener("onchange", resize);
 
 var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
     lineNumbers: true,
+	autofocus: true,
     styleActiveLine: true,
-    mode: "htmlmixed",
+    // mode: "htmlmixed",
 });
+
+editor.setSize("100%", "100%");
+// editor.focus();
 
 async function run_button_clicked() {
 	await wasm_bindgen('./pkg/editor_bg.wasm');
@@ -14,6 +22,10 @@ async function run_button_clicked() {
     const code_output = wasm_bindgen.rust_function(user_code);
     code_output_textarea.value = code_output;
 }
+
+// function resize() {
+// 	code_output_textarea.style.height = code_output_textarea.scrollHeight;
+// }
 
 
 /*
@@ -26,4 +38,7 @@ cargo clean
 cargo build
 wasm-pack build --target no-modules
 basic-http-server
+
+to look into later
+keymap & extrakeys in configuration of codemirror
 */
