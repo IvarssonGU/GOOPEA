5185db1a8cc40a8c09a3cd6bce031f85b9608176
ellalindstroem <ellalind@student.chalmers.se>
2025-03-05 16:24:58 +0100

    prelim runtime + display

--- editor/static/js/index.js
+++ editor/static/js/index.js
@@ -1,31 +1,34 @@
 let user_code_textarea = document.getElementById("editor");
 let code_output_textarea = document.getElementById("output");
-// let cm = CodeMirror.fromTextArea(user_code_textarea);
-// cm.setSize("100%", "100%");
-
-// document.addEventListener("onchange", resize);
 
 var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
     lineNumbers: true,
 	autofocus: true,
     styleActiveLine: true,
-    // mode: "htmlmixed",
 });
 
 editor.setSize("100%", "100%");
-// editor.focus();
 
 async function run_button_clicked() {
+	const startTime = performance.now();
+
 	await wasm_bindgen('./pkg/editor_bg.wasm');
+
 
     let user_code = editor.getValue();
     const code_output = wasm_bindgen.rust_function(user_code);
     code_output_textarea.value = code_output;
+
+	const endTime = performance.now();
+
+	update_runtime(endTime - startTime);
 }
 
-// function resize() {
-// 	code_output_textarea.style.height = code_output_textarea.scrollHeight;
-// }
+function update_runtime(runtime) {
+	let runtime_div = document.getElementById("runtime");
+
+	runtime_div.innerHTML = "Runtime: " + runtime + " ms";
+}
 
 
 /*
