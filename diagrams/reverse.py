dfd3ffacb572865e823d362011e87a16d57768ad
Alexander <alexander.wahlsten@gmail.com>
2025-03-06 18:04:29 +0100

    Nonfip looks great!

--- diagrams/reverse.py
+++ diagrams/reverse.py
@@ -17,6 +17,8 @@ def is_in_scope(name: str) -> bool:
 deallocated = []
 new_nodes = []
 old_vars = []
+temp_vars = []
+removed_vars = []
 rendered_frames = 0
 def render():
     global rendered_frames
@@ -78,10 +80,13 @@ def render():
         color = None
         fontcolor = None
 
-        if var in old_vars: 
-            fillcolor = "#64646430"
-            color = fillcolor
-            fontcolor = fillcolor
+        #if var in old_vars: 
+        #    fillcolor = "#64646430"
+        #    color = fillcolor
+        #    fontcolor = fillcolor
+
+        if var in removed_vars: 
+            fillcolor = "red"
     
         content_label = "Nil"
         if isinstance(vars[var], int): content_label = str(vars[var])
@@ -93,8 +98,8 @@ def render():
 
     for (a, b) in edges:
         color = "black"
-        if a in vars and a in old_vars:
-            color = "#64646430"
+        #if a in vars and a in old_vars:
+        #    color = "#64646430"
 
         if a in vars: 
             if not is_in_scope(a): continue
@@ -132,6 +137,29 @@ def render():
     if did_deallocate:
         render()
 
+    for name in removed_vars:
+        if isinstance(vars[name], str):
+            edges.remove((name, vars[name]))
+
+        scopes[-1][1].remove(name)
+
+        del vars[name]
+        del var_labels[name]
+
+        if name in temp_vars:
+            temp_vars.remove(name)
+
+    removed_vars.clear()
+
+    newly_removed = False
+    for var in temp_vars:
+        removed_vars.append(var)
+        newly_removed = True
+    temp_vars.clear()
+
+    if newly_removed:
+        render()
+
 added_identifiers = 0
 def get_new_ident() -> str:
     global added_identifiers
@@ -152,19 +180,17 @@ def cons(val: int, next: Optional[str] = None):
     if next is not None:
         edges.append((name + ":ptr", next))
 
-    var("temp", name)
-
-    render()
     return name
 
 vars = {}
 var_labels = {}
 
-def var(label: str, content: Optional[str]) -> str:
+def var(label: str, content: Optional[str], temporary: bool = False) -> str:
     name = get_new_ident()
 
     var_labels[name] = label
     scopes[-1][1].append(name)
+    if temporary: temp_vars.append(name)
 
     vars[name] = None
     set_var_content(name, content)
@@ -185,37 +211,53 @@ def push_scope(label):
     scopes.append((label, []))
 
 def remove_var(name):
-    if isinstance(vars[name], str):
-        edges.remove((name, vars[name]))
+    removed_vars.append(name)
+    render()
 
-    del vars[name]
-    del var_labels[name]
+def remove_vars(names):
+    removed_vars.extend(names)
+    render()
 
 def pop_scope():
-    gone_vars = scopes.pop()[1]
+    gone_vars = scopes[-1][1].copy()
 
     for name in gone_vars:
         remove_var(name)
 
+    scopes.pop()
+
 def reverseHelper(list: Optional[str], acc: Optional[str], depth: int, fip: bool) -> str:
     push_scope(f"ReverseHelper|{depth}")
 
-    var("list", list)
-    var("acc", acc)
+    listvar = var("list", list)
+    accvar = var("acc", acc, False)
     render()
 
+    remove_var(listvar)
+
     if list is None:
+        remove_var(accvar)
+
         pop_scope()
         return acc
     else:
         x = nodes[list][0]
         xs = nodes[list][1]
 
-        var("x", x)
-        var("xs", xs)
+        xvar = var("x", x)
+        xsvar = var("xs", xs)
         render()
 
-        res = reverseHelper(xs, cons(x, acc), depth + 1, fip)
+
+        remove_vars([accvar, xvar])
+        c = cons(x, acc)
+        cvar = var("temp", c)
+        render()
+
+        remove_vars([xsvar, cvar])
+
+        # if fip: temp_vars.append(accvar)
+        res = reverseHelper(xs, c, depth + 1, fip)
         var("return", res)
         render()
         pop_scope()
@@ -225,10 +267,10 @@ def reverseHelper(list: Optional[str], acc: Optional[str], depth: int, fip: bool
 def reverse(list: str, fip: bool) -> str:
     push_scope("Reverse")
 
-    var("list", list)
+    var("list", list, True)
     render()
 
-    reversed = reverseHelper(list, None, 0, fip)
+    reversed = reverseHelper(list, None, 1, fip)
     var("return", reversed)
     render()
 
@@ -238,17 +280,20 @@ def reverse(list: str, fip: bool) -> str:
     
 def do_reverse(fip: bool):
     push_scope("Main")
-    push_scope("Example")
 
-    lst = cons(1, cons(2, None))
+    c1 = cons(2, None)
+    var("temp", c1, True)
+    render()
 
-    reversed = reverse(lst, fip)
+    c2 = cons(1, c1)
+    var("temp", c2, True)
+    render()
+
+    reversed = reverse(c2, fip)
+
     var("return", reversed)
     render()
 
     pop_scope()
-
-    var("temp", reversed)
-    render()
 
 do_reverse(True)
