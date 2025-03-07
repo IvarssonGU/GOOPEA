3d86c711e430528b5dd68823eddf6c0f3067acf0
ellalindstroem <ellalind@student.chalmers.se>
2025-03-07 11:30:15 +0100

    added nav bar and empty example page

--- editor/static/js/index.js
+++ editor/static/js/index.js
@@ -12,8 +12,10 @@ editor.setSize("100%", "100%");
 async function run_button_clicked() {
 	const startTime = performance.now();
 
-	await wasm_bindgen('./pkg/editor_bg.wasm');
+	// let temp_output = "Cons(3, Cons(2, Cons(1, Nil)))"
+    // code_output_textarea.value = temp_output;
 
+	await wasm_bindgen('./pkg/editor_bg.wasm');
 
     let user_code = editor.getValue();
     const code_output = wasm_bindgen.rust_function(user_code);
@@ -22,6 +24,8 @@ async function run_button_clicked() {
 	const endTime = performance.now();
 
 	update_runtime(endTime - startTime);
+	// update_runtime(5);
+
 }
 
 function update_runtime(runtime) {
@@ -44,4 +48,27 @@ basic-http-server
 
 to look into later
 keymap & extrakeys in configuration of codemirror
+*/
+
+/*
+code for halftime presentation mockup:
+
+enum List = Nil, Cons(Int, List);
+    
+fip (List, List): List
+ReverseHelper(list, acc) =
+        match list {
+            Nil: acc,
+            Cons(x, xs): ReverseHelper(xs, Cons(x, acc))
+        };
+
+fip List: List
+ReverseList list = ReverseHelper(list, Nil);
+
+fip (): ()
+main = print(ReverseList(Cons(1, Cons(2, Cons(3, Nil)))));
+
+
+let temp_output = "Cons(3, Cons(2, Cons(1, Nil)))"
+code_output_textarea.value = temp_output;
 */
