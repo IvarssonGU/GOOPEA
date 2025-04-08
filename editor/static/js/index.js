f624eac1b1e189d560b9a7dba49db6d78f18a99a
ellalindstroem <ellalind@student.chalmers.se>
2025-04-08 11:56:54 +0200

    reworked tab switching, moved some stuff around

--- editor/static/js/index.js
+++ editor/static/js/index.js
@@ -1,5 +1,6 @@
 let output_textarea = document.getElementById("output");
 let debug_textarea = document.getElementById("debug");
+let messages_textarea = document.getElementById("messages");
 let c_textarea = document.getElementById("c-code");
 let diff1_textarea = document.getElementById("diff1");
 let diff2_textarea = document.getElementById("diff2");
@@ -8,9 +9,10 @@ let step2_textarea = document.getElementById("step2");
 let step3_textarea = document.getElementById("step3");
 
 let output_button = document.getElementById("output-tab-button");
-let code_button = document.getElementById("compiler-tab-button");
+let compiler_button = document.getElementById("compiler-tab-button");
 let debug_tab_button = document.getElementById("debug-tab-button");
 
+let messages_button = document.getElementById("messages-tab");
 let ccode_button = document.getElementById("ccode-tab");
 let diff_button = document.getElementById("diff-tab");
 let step1_button = document.getElementById("step1-tab");
@@ -20,6 +22,7 @@ let step3_button = document.getElementById("step3-tab");
 let diff1_select = document.getElementById("diff1-select");
 let diff2_select = document.getElementById("diff2-select");
 
+let compiler_message = "Click Compile, Run, or Debug to get a compiler message"
 let ccode_value = "Click Compile, Run, or Debug to view C code";
 let step1_value = "Click Compile, Run, or Debug to view step1";
 let step2_value = "Click Compile, Run, or Debug to view step2";
@@ -43,24 +46,27 @@ editor.setSize("100%", "100%");
 document.addEventListener("DOMContentLoaded", () => {
     output_textarea.value = "";
     c_textarea.value = "";
-    debug_textarea.style.display = 'none';
-    c_textarea.style.display = 'none';
-    diff1_textarea.style.display = 'none';
-    diff2_textarea.style.display = 'none';
-    step1_textarea.style.display = 'none';
-    step2_textarea.style.display = 'none';
-    step3_textarea.style.display = 'none';
+    // messages_textarea.style.display = 'none';
+    // debug_textarea.style.display = 'none';
+    // c_textarea.style.display = 'none';
+    // diff1_textarea.style.display = 'none';
+    // diff2_textarea.style.display = 'none';
+    // step1_textarea.style.display = 'none';
+    // step2_textarea.style.display = 'none';
+    // step3_textarea.style.display = 'none';
     diff1_selected();
     diff2_selected();
-
+    
     if ("tab" in localStorage) {
         let current_tab = localStorage.getItem("tab");
         switch (current_tab) {
             case "output": switch_tab(0); break;
-            case "code": switch_tab(1); break;
+            case "compiler": switch_tab(1); break;
             case "debug": switch_tab(2); break;
             default: switch_tab(0);
         }
+    } else {
+        switch_tab(0);
     }
 
     debug_textarea.value = "";
@@ -84,7 +90,7 @@ window.onbeforeunload = function() {
     localStorage.setItem("code", editor.getValue());
    
     if (output_button.classList.contains("current-tab")) localStorage.setItem("tab", "output");
-    if (code_button.classList.contains("current-tab")) localStorage.setItem("tab", "code");
+    if (compiler_button.classList.contains("current-tab")) localStorage.setItem("tab", "compiler");
     if (debug_tab_button.classList.contains("current-tab")) localStorage.setItem("tab", "debug");
     
     if (document.getElementById("theme-button").classList.contains("dark")) {
@@ -101,13 +107,15 @@ async function compile_and_populate() {
 
     try {
         //compilation (get all the steps and assign them to their vars here)
+        compiler_message = "looks good";
         ccode_value = wasm_bindgen.get_c_code(code);
         step1_value = "compiled step1 (not implemented yet)";
         step2_value = "compiled step2 (not implemented yet)";
         step3_value = "compiled step3 (not implemented yet)";
     } catch(error) {
         //populate error messages
-        ccode_value = "error message?";
+        compiler_message = "error message";
+        ccode_value = "error C code";
         step1_value = "error step1 (not implemented yet)";
         step2_value = "error step2 (not implemented yet)";
         step3_value = "error step3 (not implemented yet)";
@@ -116,6 +124,7 @@ async function compile_and_populate() {
     }
     
     //assign to textareas
+    messages_textarea.value = compiler_message;
     c_textarea.value = ccode_value;
     step1_textarea.value = step1_value;
     step2_textarea.value = step2_value;
@@ -141,6 +150,7 @@ async function run_button_clicked() {
 
     try {
         //get output (not implemented)
+        output_textarea.value = "output not implemented";
 
         //show only the final debug print
         wasm_bindgen.start_interpreter(code);
@@ -233,7 +243,7 @@ function save_state(opt) {
     localStorage.setItem("code", editor.getValue());
 
     if (output_button.classList.contains("current-tab")) localStorage.setItem("tab", "output");
-    if (code_button.classList.contains("current-tab")) localStorage.setItem("tab", "code");
+    if (compiler_button.classList.contains("current-tab")) localStorage.setItem("tab", "compiler");
     if (debug_tab_button.classList.contains("current-tab")) localStorage.setItem("tab", "debug");
 
     change_page(opt);
@@ -242,122 +252,111 @@ function save_state(opt) {
 //switch between the tabs on the right half of page
 function switch_tab(opt) {    
     let debug_buttons = document.getElementsByClassName("debug-button");
-    let code_step_buttons = document.getElementsByClassName("code-step-button")
+    let code_step_buttons = document.getElementsByClassName("compiler-button")
 
-    if (output_button.classList.contains("current-tab")) output_button.classList.toggle("current-tab");
-    if (code_button.classList.contains("current-tab")) {
-        code_button.classList.toggle("current-tab");
+    if (output_button.classList.contains("current-tab")) {
+        output_button.classList.toggle("current-tab");
+        output_textarea.classList.toggle("hide");
+    }
+    if (compiler_button.classList.contains("current-tab")) {
+        compiler_button.classList.toggle("current-tab");
         for (var i = 0; i < code_step_buttons.length; i++) code_step_buttons[i].classList.toggle("hide");
-        switch_code_step(0);
+        switch_compiler_tab(-2);
+        messages_textarea.classList.toggle("hide");
     }
     if (debug_tab_button.classList.contains("current-tab")) {
         debug_tab_button.classList.toggle("current-tab");
         for (var i = 0; i < debug_buttons.length; i++) debug_buttons[i].classList.toggle("hide");
+        debug_textarea.classList.toggle("hide");
     }
 
     switch (opt) {
         case 0: //switch to output
             output_button.classList.toggle("current-tab");
-            output_textarea.style.display = 'block';
-            c_textarea.style.display = 'none';
-            debug_textarea.style.display = 'none';
+            output_textarea.classList.toggle("hide");
             break;
         case 1: //switch to compiler
-            code_button.classList.toggle("current-tab");
+            compiler_button.classList.toggle("current-tab");
             for (var i = 0; i < code_step_buttons.length; i++) code_step_buttons[i].classList.toggle("hide");
-            c_textarea.style.display = 'block'
-            output_textarea.style.display = 'none';
-            debug_textarea.style.display = 'none';
+            messages_textarea.classList.toggle("hide");
             break;
         case 2: //switch to debug
             debug_tab_button.classList.toggle("current-tab");
             for (var i = 0; i < debug_buttons.length; i++) debug_buttons[i].classList.toggle("hide");
-            debug_textarea.style.display = 'block';
-            c_textarea.style.display = 'none';
-            output_textarea.style.display = 'none';
+            debug_textarea.classList.toggle("hide");
             break;
         default:
             output_button.classList.toggle("current-tab");
-            output_textarea.style.display = 'block';
-            c_textarea.style.display = 'none';
-            debug_textarea.style.display = 'none';
+            output_textarea.classList.toggle("hide");
 
     }
 }
 
-function switch_code_step(opt) {
-
-    if (ccode_button.classList.contains("current-tab")) ccode_button.classList.toggle("current-tab");
+function switch_compiler_tab(opt) {
+    if (messages_button.classList.contains("current-tab")) {
+        messages_button.classList.toggle("current-tab");
+        messages_textarea.classList.toggle("hide");
+        if (messages_textarea.classList.contains("hide")) console.log("hidden");
+    }
+    if (ccode_button.classList.contains("current-tab")) {
+        ccode_button.classList.toggle("current-tab");
+        c_textarea.classList.toggle("hide");
+    }
     if (diff_button.classList.contains("current-tab")) {
         diff_button.classList.toggle("current-tab");
         document.getElementById("diff-container").classList.toggle("hide");
     }
-    if (step1_button.classList.contains("current-tab")) step1_button.classList.toggle("current-tab");
-    if (step2_button.classList.contains("current-tab")) step2_button.classList.toggle("current-tab");
-    if (step3_button.classList.contains("current-tab")) step3_button.classList.toggle("current-tab");
+    if (step1_button.classList.contains("current-tab")) {
+        step1_button.classList.toggle("current-tab");
+        step1_textarea.classList.toggle("hide");
+    }
+    if (step2_button.classList.contains("current-tab")) {
+        step2_button.classList.toggle("current-tab");
+        step2_textarea.classList.toggle("hide");
+    }
+    if (step3_button.classList.contains("current-tab")) {
+        step3_button.classList.toggle("current-tab");
+        step3_textarea.classList.toggle("hide");
+    }
 
     switch (opt) {
-        case 0: //switch to ccode
-            ccode_button.classList.toggle("current-tab");
-            c_textarea.style.display = 'block';
-            diff1_textarea.style.display = 'none'
-            diff2_textarea.style.display = 'none'
-            step1_textarea.style.display = 'none';
-            step2_textarea.style.display = 'none';
-            step3_textarea.style.display = 'none';
+        case -2: //switch to messages
+            messages_button.classList.toggle("current-tab");
+            messages_textarea.classList.toggle("hide");
             break;
         case -1: //switch to diff
             diff_button.classList.toggle("current-tab");
             document.getElementById("diff-container").classList.toggle("hide");
-            c_textarea.style.display = 'none';
-            diff1_textarea.style.display = 'block'
-            diff2_textarea.style.display = 'block'
-            step1_textarea.style.display = 'none';
-            step2_textarea.style.display = 'none';
-            step3_textarea.style.display = 'none';
+            break;
+        case 0: //switch to c code
+            ccode_button.classList.toggle("current-tab");
+            c_textarea.classList.toggle("hide");
             break;
         case 1: //switch to step1
             step1_button.classList.toggle("current-tab");
-            c_textarea.style.display = 'none';
-            diff1_textarea.style.display = 'none'
-            diff2_textarea.style.display = 'none'
-            step1_textarea.style.display = 'block';
-            step2_textarea.style.display = 'none';
-            step3_textarea.style.display = 'none';
+            step1_textarea.classList.toggle("hide");
             break;
         case 2: //switch to step2
             step2_button.classList.toggle("current-tab");
-            c_textarea.style.display = 'none';
-            diff1_textarea.style.display = 'none'
-            diff2_textarea.style.display = 'none'
-            step1_textarea.style.display = 'none';
-            step2_textarea.style.display = 'block';
-            step3_textarea.style.display = 'none';
+            step2_textarea.classList.toggle("hide");
             break;
         case 3: //switch to step3
             step3_button.classList.toggle("current-tab");
-            c_textarea.style.display = 'none';
-            diff1_textarea.style.display = 'none'
-            diff2_textarea.style.display = 'none'
-            step1_textarea.style.display = 'none';
-            step2_textarea.style.display = 'none';
-            step3_textarea.style.display = 'block';
+            step3_textarea.classList.toggle("hide");
             break;
         default:
             ccode_button.classList.toggle("current-tab");
-            c_textarea.style.display = 'block';
-            diff1_textarea.style.display = 'none'
-            diff2_textarea.style.display = 'none'
-            step1_textarea.style.display = 'none';
-            step2_textarea.style.display = 'none';
-            step3_textarea.style.display = 'none';
+            c_textarea.classList.toggle("hide");
 
     }
 }
 
-//dropdowns under compiler/diff view
+//dropdowns under compiler->diff view
 function diff1_selected() {
     switch(diff1_select.value) {
+        case "c":
+            diff1_textarea.value = ccode_value;
+            break;
         case "step1":
             diff1_textarea.value = step1_value;
             break;
@@ -373,6 +372,9 @@ function diff1_selected() {
 }
 function diff2_selected() {
     switch(diff2_select.value) {
+        case "c":
+            diff2_textarea.value = ccode_value;
+            break;
         case "step1":
             diff2_textarea.value = step1_value;
             break;
