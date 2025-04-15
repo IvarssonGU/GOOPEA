3a86faab585a62a93ecd943d1cf80b5c3b1e13cb
Alexander <alexander.wahlsten@gmail.com>
2025-04-15 16:04:27 +0200

    Turned them into boxes and added some links between

--- editor/static/js/visualization.js
+++ editor/static/js/visualization.js
@@ -3,7 +3,11 @@ let visualization_containter = document.getElementById("visualization");
 const width = 500;
 const height = 500;
 
-let graphical_nodes = []
+const box_height = 50;
+const box_field_width = 50;
+
+let nodes = []
+let links = []
 
 // Create the SVG container.
 const svg = d3.create("svg")
@@ -12,21 +16,33 @@ const svg = d3.create("svg")
     .attr("style", "max-width: 100%; height: auto;");
 
 const simulation = d3.forceSimulation()
-    .force("charge", d3.forceManyBody())
+    .force("link", d3.forceLink(links).id(d => d.id).distance(200))
+    .force("charge", d3.forceManyBody().strength(-200))
     .force("x", d3.forceX(width / 2))
     .force("y", d3.forceY(height / 2))
     .on("tick", ticked);
 
-function ticked() {
-    node
-        .attr("cx", d => d.x)
-        .attr("cy", d => d.y);
-}
+let link = svg.append("g")
+    .attr("stroke", "#999")
+    .attr("stroke-opacity", 0.6)
+    .selectAll("line");
 
 let node = svg.append("g")
     .attr("stroke", "#fff")
     .attr("stroke-width", 1.5)
     .selectAll("circle");
+
+function ticked() {
+    node
+        .attr("x", d => d.x - d.width / 2)
+        .attr("y", d => d.y - box_height / 2);
+
+    link
+        .attr("x1", d => d.source.x)
+        .attr("y1", d => d.source.y)
+        .attr("x2", d => d.target.x)
+        .attr("y2", d => d.target.y);
+}
 
 function dragstarted(event) {
     if (!event.active) simulation.alphaTarget(0.3).restart();
@@ -54,43 +70,76 @@ function update_visualization() {
     const mem = wasm_bindgen.take_interpreter_memory_snapshot()
     console.log(mem)
 
-    while (graphical_nodes.length < mem.heap.length) {
-        graphical_nodes.push({ x: 0, y: height / 2 })
+    while (nodes.length < mem.heap.length) {
+        nodes.push({ x: 0, y: height / 2, id: nodes.length })
     }
 
-    while (graphical_nodes.length > mem.heap.length) {
-        graphical_nodes.pop()
+    while (nodes.length > mem.heap.length) {
+        nodes.pop()
     }
 
-    node = node.data(graphical_nodes);
-    let was_changed = !node.enter().empty();
+    for (let i = 0; i < mem.heap.length; i++) {
+        nodes[i].fields = mem.heap[i]
+        nodes[i].width = mem.heap[i].length * box_field_width
+    }
+
+    node = node.data(nodes);
+    let was_changed = !node.enter().empty() || !node.exit().empty();
 
     node.exit()
         .transition()
             .attr("fill", "red")
         .transition()
-            .attr("r", 0)
+            .attr("opacity", 0)
             .remove()
 
-    node = node.enter().append("circle")
+    node = node.enter().append("rect")
         .call(d3.drag()
             .on("start", dragstarted)
             .on("drag", dragged)
             .on("end", dragended))
-        .attr("r", 0)
+        .attr("width", x => x.width)
+        .attr("height", box_height)
+        .attr("opacity", 0)
         .attr("fill", "green")
+        .attr("stroke", "#333")
         .attr("class", "node")
         .transition()
-            .attr("r", 10)
+            .attr("opacity", 1)
         .transition()
             .attr()
-            .attr("fill", "black")
+            .attr("fill", "#f0f0f0")
             .duration(2000)
             .selection()
         .merge(node);
 
+    links = []
+    for([i, fields] of mem.heap.entries()) {
+        for(data of fields) {
+
+            if (!data.is_ptr) { continue }
+
+            links.push({
+                source: i,
+                target: data.val
+            })
+        }
+    }
+
+    link = link.data(links, d => d.source.id + "-" + d.target.id);
+    was_changed = was_changed || !link.enter().empty() || !link.exit().empty();
+
+    link.exit().remove();
+    link = link.enter().append("line")
+      .attr("class", "link")
+      .merge(link);
+
+    console.log(links)
+
+    
     if (was_changed) {
-        simulation.nodes(graphical_nodes)
+        simulation.force("link").links(links);
+        simulation.nodes(nodes)
         simulation.alpha(1).restart();
     }
 
