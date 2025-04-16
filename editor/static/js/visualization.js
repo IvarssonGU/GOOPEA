af732fd6358ce7ad2b7470df583056b470532639
Alexander <alexander.wahlsten@gmail.com>
2025-04-16 18:51:50 +0200

    Fixed bug and added animation when data gets edited

--- editor/static/js/visualization.js
+++ editor/static/js/visualization.js
@@ -6,6 +6,8 @@ const height = 500;
 const box_height = 50;
 const box_field_width = 50;
 
+const box_color = "#f0f0f0"
+
 let nodes = []
 
 // Create the SVG container.
@@ -69,13 +71,14 @@ visualization_containter.append(svg.node())
 
 function update_visualization() {
     const mem = wasm_bindgen.take_interpreter_memory_snapshot()
+    console.log(mem.heap)
 
     const graph = d3.graph()
 
-    let graph_nodes = mem.heap.map(fields => graph.node(fields))
+    let graph_nodes = mem.heap.map((fields, i) => graph.node({ fields: fields, id: i }))
     
     for(const node of graph_nodes) {
-        for(const [i, field] of node.data.entries()) {
+        for(const [i, field] of node.data.fields.entries()) {
             if(field.is_ptr) {
                 graph.link(node, graph_nodes[field.val], i)
             }
@@ -83,23 +86,17 @@ function update_visualization() {
     }
 
     const { width: graph_width, height: graph_height } = d3.sugiyama()
-        .nodeSize(d => [10 + box_field_width * d.data.length, box_height])
+        .nodeSize(d => [10 + box_field_width * d.data.fields.length, box_height])
         .gap([20, 50])
         .tweaks([/*d3.tweakFlip("diagonal"), */tweak_endpoints])(graph)
 
 
     const zoom_scale = Math.min(width / graph_width, height / graph_height);
 
-
-    console.log(graph_width)
-    console.log(graph_height)
-
-    const line = d3.linkVertical().x(d => d[0]).y(d => d[1]);
-
     function transform_node(selection) {
         selection
-        .attr("width", d => box_field_width * d.data.length)
-        .attr("transform", d => `translate(${d.x - (box_field_width * d.data.length) / 2},${d.y - box_height / 2})`)
+        .attr("width", d => box_field_width * d.data.fields.length)
+        .attr("transform", d => `translate(${d.x - (box_field_width * d.data.fields.length) / 2},${d.y - box_height / 2})`)
     }
 
     while (nodes.length < mem.heap.length) {
@@ -117,11 +114,15 @@ function update_visualization() {
 
     let was_changed = false;
 
+    console.log(graph.nnodes())
+
     d3.select(".nodes")
-        .selectAll("g")
-        .data(graph.nodes())
+        .selectAll(".node")
+        .data(graph.nodes(), d => d.data.id)
         .join(
             function(enter) {
+                console.log("Enter: " + enter.size())
+
                 was_changed |= !enter.empty();
 
                 return enter.append("g")
@@ -141,45 +142,21 @@ function update_visualization() {
                         .delay(500)
                     .transition()
                         .attr()
-                        .attr("fill", "#f0f0f0")
+                        .attr("fill", box_color)
                         .duration(2000)
                     .selection()
-                    .each(function(p, _) {
-                        d3.select(this)
-                            .selectAll("rect")
-                            .data(p.data)
-                            .join(enter => 
-                                enter
-                                .append("g")
-                                .attr("transform", (_,i) => `translate(${i * box_field_width},0)`)
-                                .each(function(data, i) {
-                                    let rect = d3.select(this).append("rect")
-                                        .attr("width", box_field_width)
-                                        .attr("height", box_height);
-
-                                    if (i < 3) {
-                                        rect.attr("stroke-dasharray", "5")
-                                    }
-
-                                    if (!data.is_ptr) {
-                                        d3.select(this).append("text")
-                                            .attr("x", box_field_width / 2)
-                                            .attr("y", box_height / 2)
-                                            .attr("text-anchor", "middle")
-                                            .attr("alignment-baseline", "central")
-                                            .text(d => d.val)
-                                    }
-                                }) 
-                            )
-                    })
             },
             function(update) { 
+                console.log("Update: " + update.size())
+                
                 update
                     .transition(1)
                     .delay(250)
                     .call(transform_node)
             },
             function(exit) {
+                console.log("Exit: " + exit.size())
+
                 was_changed |= !exit.empty();
 
                 exit.transition()
@@ -189,6 +166,65 @@ function update_visualization() {
                     .remove()
             }
         );
+
+    function update_text(selection) {
+        selection.text(d => {
+            if(d.is_ptr) {
+                return ""
+            } else {
+                return d.val
+            }
+        })
+    }
+
+    d3.select(".nodes")
+        .selectAll(".node")
+        .each(function(p, _) {
+            d3.select(this)
+                .selectAll("g")
+                .data(p.data.fields)
+                .join(function(enter) { 
+                    return enter.append("g")
+                        .attr("transform", (_,i) => `translate(${i * box_field_width},0)`)
+                        .each(function(data, i) {
+                            this.__prev_val = data.val;
+
+                            let rect = d3.select(this).append("rect")
+                                .attr("width", box_field_width)
+                                .attr("height", box_height);
+
+                            if (i < 3) {
+                                rect.attr("stroke-dasharray", "5")
+                            }
+
+                            d3.select(this).append("text")
+                                .attr("x", box_field_width / 2)
+                                .attr("y", box_height / 2)
+                                .attr("text-anchor", "middle")
+                                .attr("alignment-baseline", "central")
+                                .call(update_text)
+                        })
+                    },
+                    function(update) {
+                        update.each(function(data, i ) {
+                            if(this.__prev_val != data.val) {
+                                d3.select(this).select("text").call(update_text)
+
+                                d3.select(this).select("rect")
+                                    .attr("fill", "orange")
+                                    .transition()
+                                        .delay(500)
+                                        .duration(1000)
+                                        .attr("fill", box_color)
+
+                                this.__prev_val = data.val
+                            }
+
+
+                        })
+                    }
+                )
+        })
     
     svg
         .select(".links")
@@ -212,12 +248,20 @@ function update_visualization() {
                     .delay(500)
             },
             function (update) {
-
+                update
+                    .transition(1)
+                    .delay(250)
+                    .attr("d", ({ points }) =>
+                        d3.linkVertical()({
+                          source: points[0],
+                          target: points[1]
+                        })
+                    )
+                    
             },
             function (exit) {
                 exit
                     .transition()
-                    .delay(250)
                     .attr("opacity", 0)
                     .remove()
             }
@@ -270,7 +314,7 @@ function update_visualization() {
 function tweak_endpoints(graph, size) {
     for(let link of graph.links()) {
         let [sx, sy] = link.points[0]
-        sx -= link.source.data.length * box_field_width / 2
+        sx -= link.source.data.fields.length * box_field_width / 2
         sx += (link.data + 0.5) * box_field_width
         sy += box_height / 2
         link.points[0] = [sx, sy]
